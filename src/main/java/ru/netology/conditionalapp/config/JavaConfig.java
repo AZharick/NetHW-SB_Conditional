dfa1e733ce16bc232b7b7cda398bb656d21ceb4f
AZharick <bopohlabs@mail.ru>
2023-12-30 18:03:10 +0300

    Initial commit

---
+++ src/main/java/ru/netology/conditionalapp/config/JavaConfig.java
@@ -0,0 +1,23 @@
+package ru.netology.conditionalapp.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.conditionalapp.systemProfile.DevProfile;
+import ru.netology.conditionalapp.systemProfile.ProductionProfile;
+import ru.netology.conditionalapp.systemProfile.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+    @Bean
+    @ConditionalOnProperty(prefix = "netology.profile", name = "dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "netology.profile", name = "dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
